9434e7f8baae2246c05b691914a06da048b1b990
Trung <duongtoantrung@gmail.com>
2024-01-11 11:28:42 +0700

    XuLyLogicQLNV

--- QLNhanVien_5tuan/QuanLyNhanVien1/QuanLyNhanVien.cs
+++ QLNhanVien_5tuan/QuanLyNhanVien1/QuanLyNhanVien.cs
@@ -8,7 +8,7 @@ namespace QuanLyNhanVien1
 {
     internal class QuanLyNhanVien
     {
-        // Các thuộc tính private
+        // Các thuộc tính private=
         public int maNV { get; set; }
         public string tenNV { get; set; }
         public DateTime ngaysinh { get; set; }
@@ -22,138 +22,314 @@ namespace QuanLyNhanVien1
         // Constructor mặc định
         public QuanLyNhanVien()
         {
-          
+
         }
+
 
         // Constructor có truyền thông số
-        public QuanLyNhanVien(string sID, string hoTen, int birthYear, bool gender, string phongBan, string trinhDo, string capBac, int luongCoBan, float heSo, global::System.Int32 maNV, global::System.String tenNV, DateTime ngaysinh, global::System.String gioitinh, global::System.String diachi, DateTime ngayvaolam, global::System.String bangcap, global::System.String hinhthucNV)
+        public QuanLyNhanVien(int maNV, string tenNV, DateTime ngaySinh, bool gioiTinh, string diaChi, DateTime ngayVaoLam, string bangCap, string hinhThucNV)
         {
             // Kiểm tra năm sinh 
-            if (birthYear > DateTime.Now.Year - 18)
+            if (ngaySinh.Year > DateTime.Now.Year - 18)
             {
-                throw new ArgumentException("năm sinh không hợp lệ");
-            }
-            //kiểm tra hệ số lương 
-            if (heSo <= 0 || heSo >= 5)
-            {
-                throw new ArgumentException("Hệ số lương không hợp lệ");
+                throw new ArgumentException("Năm sinh không hợp lệ");
             }
 
-            // Gán giá trị
-
-            tthis.maNV = maNV;
-            this.tenNV = tenNV;
-            this.ngaysinh = ngaysinh;
-            this.gioitinh = gioitinh;
-            this.diachi = diachi;
-            this.ngayvaolam = ngayvaolam;
-            this.bangcap = bangcap;
-            this.hinhthucNV = hinhthucNV;
-
+        
+            MaNV = maNV;
+            TenNV = tenNV;
+            NgaySinh = ngaySinh;
+            GioiTinh = gioiTinh;
+            DiaChi = diaChi;
+            NgayVaoLam = ngayVaoLam;
+            BangCap = bangCap;
+            HinhThucNV = hinhThucNV;
         }
-        public double timeworking(int start, int end)
-        {
-            // Thời gian làm vc = end - start.
-            // thgian < 8h 
-            // 1. nếu trễ <1.5 tiếng ngày làm vc được tính 1 ngày 
-            // 2. nếu trễ 1.5-2h ngày làm vc được tính 1/2 ngày
-            // thgian làm vc từ 8-12 tiếng: mỗi giờ thêm được tính bằng 2 giờ làm bình thường
 
+
+        /*Hàm tính thời gian làm việc 
+          nhận 2 giá trị : thời gian bắt đầu vào làm , thời gian ra về 
+          trả về giá trị : thời gian nhân viên làm việc trong cty
+        */
+        public double TinhWorkingTime(DateTime start, DateTime end)
+        {
+            /*Thời gian làm việc = end - start.
+            Nếu ngày làm vc < 8h 
+               1. Nếu trễ <1.5 tiếng ngày làm vc được tính 1 ngày 
+               2. Nếu trễ 1.5-2h ngày làm vc được tính 1/2 ngày
+            Ngày làm vc từ 8-12 tiếng: mỗi giờ thêm được tính bằng 2 giờ làm bình thường
+            */
+
+            TimeSpan duration = end - start;
+
+            double totalWorkingTime = duration.TotalHours;
+
+            if (totalWorkingTime < 8)
+            {
+                if (totalWorkingTime >= 6.5)
+                {
+                    return 0.5;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            else
+            {
+                int extraHours = (int)Math.Ceiling(totalWorkingTime - 8);
+                return 8 + extraHours * 2;
+            }
+        }
+
+
+        /*Hàm tính ngày nghỉ phép của nhân viên 
+          nhận 2 giá trị : hoàn cảnh cá nhân , ngày nghỉ nv đã sử dụng
+          trả về giá trị : ngày nghỉ phép hợp lệ của nhân viên 
+          Ngoài ra : đã bổ sung 2 quy định từ công ty 
+        */
+        public double TinhPhep(string dieuKien, int ngayNghi )
+        {
+            /* 
+			Nếu thâm niên >= 12 thì 
+			    1. Điều kiện bình thương có ngayphep là 12
+			    2. điều kiện đặc biệt có ngayphep là 14
+			    3. điều kiện nặng nhọc có ngày phép là 16
+			Ngược lại thâm niên <12 thì ngayphep = thamNien
+			    nếu nghỉ quá ngày phép thì tiền phạt = bằng 20% lương tháng
+			    nếu ngày nghỉ <0 (tức là đi làm thêm ca) thì những ngày làm thêm lương = 200% lương thông thường
+        */
+
+            int thamNien = DateTime.Now.Year - NgayVaoLam.Year;
+            double ngayPhep = 0;
+            int luongThang = TinhLuong();
+            if (thamNien >= 12)
+            {
+                switch (dieuKien)
+                {
+                    case "Điều kiện bình thường":
+                        ngayPhep = 12;
+                        break;
+                    case "Điều kiện đặc biệt":
+                        ngayPhep = 14;
+                        break;
+                    case "Điều kiện nặng nhọc":
+                        ngayPhep = 16;
+                        break;
+                }
+            }
+            else
+            {
+                return ngayPhep;
+            }
+
+            if (ngayNghi > ngayPhep)
+            {
+                return luongThang * 0.2;
+
+            }
+            else if (ngayNghi < 0)
+            {
+                return luongThang * 2;
+            }
             
-
-
         }
 
-        public double Tinhphep(string dieuKien, int ngayNghi)
-        {
-            // Nếu thâm niên >= 12 thì 
-            // 1. Điều kiện bình thương có ngayphep là 12
-            // 2. điều kiện đặc biệt có ngayphep là 14
-            //3. điều kiện nặng nhọc có ngày phép là 16
-            // Ngược lại thâm niên <12 thì ngayphep = thamNien
-            // nếu nghỉ quá ngày phép thì tiền phạt = bằng 20% lương tháng
-            // nếu ngày nghỉ <0 (tức là đi làm thêm ca) thì những ngày làm thêm lương = 200% lương thông thường
-        }
+
+        /*
+         Hàm tính phụ cấp/ trợ cấp của nhân viên 
+         không cần nhận giá trị đầu vào !
+         trả về giá trị : số tiền đã được phụ cấp        
+       */
 
         public double TinhPhuCap()
         {
-            //Hocvi // = THPT Phụ cấp 0;  // trung cấp phụ cấp 2000, // cao đẳng phụ cấp 4000, đại học phụ cấp 6000 // thạc sỉ phụ cấp 8000 // tiến sĩ phụ cấp 10000
-            // Chức danh: "Nhan vien": phụ cấp chức danh = 2000; "Pho truong phong":phụ cấp chức danh  = 5000;  "Truong phong": phụ cấp = 10000;
-            // phụ cấp phòng ban: Kinh doanh phụ cấp 5000; "Ke toan phụ cấp  5000; Ban giam docphụ cấp  = 20000; "Hanh chanh"phụ cấp 10000;  "Bao ve" phụ cấp1000;
-            // luongThang = LCB + luongThamNien + luongHocVi + luongChucDanh + luongPhongBan;           
+
+            /* 
+            Học vị :THPT Phụ cấp 0; 
+                    trung cấp phụ cấp 2000
+                    cao đẳng phụ cấp 4000
+                    đại học phụ cấp 6000 
+                    thạc sỉ phụ cấp 8000 
+                    tiến sĩ phụ cấp 10000
+
+            Chức danh: "Nhan vien": phụ cấp chức danh = 2000; 
+                       "Pho truong phong":phụ cấp chức danh  = 5000;
+                       "Truong phong": phụ cấp = 10000;
+
+            phụ cấp phòng ban: Kinh doanh phụ :cấp 5000;
+                               Ke toan phụ :cấp  5000; 
+                               Ban giam doc :phụ cấp  20000;
+                               Hanh chanh :phụ cấp 10000; 
+                               Bao ve     : phụ cấp 1000;
+
+            Dòng này để trong hàm tính Lương nhé!
+            luongThang = LCB + luongThamNien + luongHocVi + luongChucDanh + luongPhongBan;           
+            */
+            int luongThang = TinhLuong();
+
+            // Phụ cấp theo học vị
+            int phuCapHocVi = 0;
+            switch (BangCap)
+            {
+                case "Phụ cấp":
+                    phuCapHocVi = 0;
+                    break;
+                case "Trung cấp":
+                    phuCapHocVi = 2000;
+                    break;
+                case "Cao đẳng":
+                    phuCapHocVi = 4000;
+                    break;
+                case "Đại học":
+                    phuCapHocVi = 6000;
+                    break;
+                case "Thạc sĩ":
+                    phuCapHocVi = 8000;
+                    break;
+                case "Tiến sĩ":
+                    phuCapHocVi = 10000;
+                    break;
+            }
+
+            // Phụ cấp theo chức danh
+            int phuCapChucDanh = 0;
+            switch (HinhThucNV)
+            {
+                case "Nhân viên":
+                    phuCapChucDanh = 2000;
+                    break;
+                case "Phó trưởng phòng":
+                    phuCapChucDanh = 5000;
+                    break;
+                case "Trưởng phòng":
+                    phuCapChucDanh = 10000;
+                    break;
+            }
+
+            // Phụ cấp theo phòng ban
+            int phuCapPhongBan = 0;
+            switch (HinhThucNV)
+            {
+                case "Kinh doanh":
+                    phuCapPhongBan = 5000;
+                    break;
+                case "Kế toán":
+                    phuCapPhongBan = 5000;
+                    break;
+                case "Ban giám đốc":
+                    phuCapPhongBan = 20000;
+                    break;
+                case "Hành chính":
+                    phuCapPhongBan = 10000;
+                    break;
+                case "Bảo vệ":
+                    phuCapPhongBan = 1000;
+                    break;
+            }
+
+            return phuCapHocVi + phuCapChucDanh + phuCapPhongBan;
+        }
+
+        
+        public double TinhLuongParttime(string loaiCV)
+        {
+            /*
+                loaiCV là "Van phong": lương partime 19000;
+             
+                loaiCV  "San xuat":luongParttime = 20000;
+            */
+            
+            switch (loaiCV)
+            {
+                case "Văn phòng":
+                    return 19000;
+                case "Sản xuất":
+                    return 20000;
+                default:
+                    return 0;
+            }
         }
 
 
-        public double TinhluongParttime(string loaiCV)
-        {
-            //loaiCV là "Van phong": lương partime 19000;
-            //loaiCV  "San xuat":luongParttime = 20000;
-        }
 
-        public double Tinhluong()
+        public double TinhLuong()
         {
-            // Số giờ làm được tính = giờ làm - ngày nghỉ phép 
-            //    luong = luongGio * gioLam + Phucap - thue;
+            /*
+             Số giờ làm được tính = giờ làm - ngày nghỉ phép 
+             luong = luongGio * gioLam + Phucap - thue;
+            */
+            double luongGio = LuongCoBan / 23.83;
+            double soGioLam = TinhWorkingTime(ngayvaolam, DateTime.Now)-TinhPhep();
+            double phuCap = TinhPhuCap();
+
+            double luong = luongGio * soGioLam + phuCap - TinhThue();
+
+            return luong;
         }
 
         public double TinhThue()
         {
-            //BHXH = luongThang * 8 / 100;
-            //BHYT = luongThang * 1.5 / 100;
-            //BHTN = luongThang * 1 / 100;
-            //TNCN = luongThang * 10 / 100;
-            //thue = BHXH + BHYT + BHTN + TNCN;
+            /*
+                BHXH = luongThang * 8 / 100;
+                BHYT = luongThang * 1.5 / 100;
+                BHTN = luongThang * 1 / 100;
+                TNCN = luongThang * 10 / 100;
+                thue = BHXH + BHYT + BHTN + TNCN;
+            */
+            double luongThang = TinhLuong();
+            double BHXH = luongThang * 8 / 100;
+            double BHYT = luongThang * 1.5 / 100;
+            double BHTN = luongThang * 1 / 100;
+            double TNCN = luongThang * 10 / 100;
+            double thue = BHXH + BHYT + BHTN + TNCN;
 
+            return thue;
         }
 
-        // Hàm Input
+        // Hàm Input từ buoi 1
         public void Input()
         {
             Console.WriteLine("Nhập thông tin nhân viên:");
             Console.Write("Mã nhân viên: ");
-            SID = Console.ReadLine();
+            MaNV = int.Parse(Console.ReadLine());
             Console.Write("Họ tên: ");
-            HoTen = Console.ReadLine();
-            bool isValidBirthYear = false;
-            while (!isValidBirthYear)
-            {
-                Console.Write("Năm sinh: ");
-                if (int.TryParse(Console.ReadLine(), out int inputBirthYear))
-                {
-                    if (DateTime.Now.Year - inputBirthYear > 18)
-                    {
-                        birthYear = inputBirthYear;
-                        isValidBirthYear = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Nhân viên phải lớn hơn 18 tuổi. Hãy nhập lại.");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Vui lòng nhập một số nguyên cho năm sinh.");
-                }
-            }
-
+            TenNV = Console.ReadLine();
+            Console.Write("Ngày sinh (dd/mm/yyyy): ");
+            NgaySinh = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+            Console.Write("Giới tính (Nam/Nu): ");
+            GioiTinh = Console.ReadLine().ToLower() == "nam";
+            Console.Write("Địa chỉ: ");
+            DiaChi = Console.ReadLine();
+            Console.Write("Ngày vào làm (dd/mm/yyyy): ");
+            NgayVaoLam = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+            Console.Write("Bằng cấp: ");
+            BangCap = Console.ReadLine();
+            Console.Write("Hình thức làm việc: ");
+            HinhThucNV = Console.ReadLine();
         }
 
-        // Hàm PrintInfor
+        // Hàm PrintInfor từ buổi 1
         public void PrintInfor()
         {
             Console.WriteLine("Thông tin nhân viên:");
-            Console.WriteLine($"Mã nhân viên: {SID}");
-            Console.WriteLine($"Họ tên: {HoTen}");
-            Console.WriteLine($"Năm sinh: {BirthYear}");
+            Console.WriteLine($"Mã nhân viên: {MaNV}");
+            Console.WriteLine($"Họ tên: {TenNV}");
+            Console.WriteLine($"Ngày sinh: {NgaySinh.Day}/{NgaySinh.Month}/{NgaySinh.Year}");
+            Console.WriteLine($"Giới tính: {(GioiTinh ? "Nam" : "Nữ")}");
+            Console.WriteLine($"Địa chỉ: {DiaChi}");
+            Console.WriteLine($"Ngày vào làm: {NgayVaoLam.Day}/{NgayVaoLam.Month}/{NgayVaoLam.Year}");
+            Console.WriteLine($"Bằng cấp: {BangCap}");
+            Console.WriteLine($"Hình thức làm việc: {HinhThucNV}");
         }
 
-        // Hàm setTrinhDo
+        // Hàm SetTrinhDo
         public bool SetTrinhDo(string trinhDo)
         {
-            // Kiểm tra điều kiện và thực hiện
-            if (trinhDo == "Đại Học " || trinhDo == "Cao Đẳng" || trinhDo == "12/12")
+            
+            if (trinhDo == "Đại học" || trinhDo == "Cao đẳng" || trinhDo == "12/12")
             {
-                this.TrinhDo = trinhDo;
+                BangCap = trinhDo;
                 return true;
             }
             else
@@ -162,25 +338,21 @@ namespace QuanLyNhanVien1
             }
         }
 
-        // Hàm setCapBac
-        public bool SetCapBac(string capBac)
+        
+        public bool SetHinhThucNV(string hinhThucNV)
         {
             // Kiểm tra điều kiện và thực hiện
-            if (capBac == "Intern" || capBac == "Senior" || capBac == "Leader")
+            if (hinhThucNV == "Nhân viên" || hinhThucNV == "Phó trưởng phòng" || hinhThucNV == "Trưởng phòng")
             {
-                this.CapBac = capBac;
+                HinhThucNV = hinhThucNV;
                 return true;
             }
             else
             {
                 return false;
             }
-        }
-
-        // Hàm tinhLuong
-        public float TinhLuong()
-        {
-            return LuongCoBan * HeSo;
         }
     }
+
 }
+
